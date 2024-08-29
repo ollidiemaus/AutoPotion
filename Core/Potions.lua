d9dbdcd8415ffd15ce51fadd46baa9531c47d02c
ollidiemaus <ollidiemaus@googlemail.com>
2024-08-29 13:02:04 +0200

    added Demonic Healthstone

--- Core/Potions.lua
+++ Core/Potions.lua
@@ -7,6 +7,7 @@ local isCata = (WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC)
 --ham.leywine = ham.Item.new(194684,"Azure Leywine")
 --ham.healthstone = ham.Item.new(117, "Healthstone")
 ham.healthstone = ham.Item.new(5512, "Healthstone")
+ham.demonicHealthstone = ham.Item.new(224464, "Demonic Healthstone") ---1 Minute CD
 ham.algariHealingPotionR3 = ham.Item.new(211880, "Algari Healing Potion")
 ham.algariHealingPotionR2 = ham.Item.new(211879, "Algari Healing Potion")
 ham.algariHealingPotionR1 = ham.Item.new(211878, "Algari Healing Potion")
