99916226919f2dd03068d81dd89ab6129f5f6f13
Oliver Maus <ollidiemaus@googlemail.com>
2026-09-21 13:50:47 +0200

    Add Rapid Regeneration (Troll racial) for Forever
    New Troll self-heal introduced in WoW Forever, spell ID 1260270:
    channeled, restores ~50% of max health over ~6s, canceled by damage,
    movement or other actions. 3 minute cooldown. Confirmed via Wowhead's
    Forever database and Method.gg's Forever racial ability roundup.

--- Core/Spells/Forever.lua
+++ Core/Spells/Forever.lua
@@ -1,6 +1,7 @@
 local addonName, ham = ...
 if not ham.isForever then return end
 
--- Forever-only spells go here, e.g.:
--- ham.someForeverOnlySpell = ham.Spell.new(123456)
--- table.insert(ham.supportedSpells, ham.someForeverOnlySpell)
+-- Troll racial (new in Forever): channeled self-heal restoring ~50% of max health over ~6s,
+-- canceled by taking damage, moving or acting. 3 minute cooldown.
+ham.rapidRegeneration = ham.Spell.new(1260270)
+table.insert(ham.supportedSpells, ham.rapidRegeneration)
