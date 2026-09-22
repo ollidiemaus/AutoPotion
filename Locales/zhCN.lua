2c55daa4e8691eb9c32394e3826f876710036b82
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2026-09-22 15:27:45 +0800

    Update zhCN.lua (#120)

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -11,6 +11,11 @@ L["Configure the behavior of the addon. IE: if you want to include class spells"
 "您可以在此设置技能。例如：是否包含职业/种族回血技能"
 L["Current Priority"] = "当前优先级"
 L["Bandage Priority"] = "绷带优先级"
+L["Food Priority"] = "食物优先级"
+L["Drink Priority"] = "饮料优先级"
+L["Include Buff Food"] = "包含增益食物"
+L["Include \"Well Fed\"/\"Relaxed\" buff food and drink items (with situational secondary-stat bonuses) in the food and drink priority lists."] =
+"将有副属性加成的 \"进食充分\"/\"悠闲\" 增益食物与饮料纳入食物和饮料优先列表。"
 L["Heartseeking Health Injector (tinker)"] = "觅心生命注射器（匠械）"
 L["Include /stopcasting in the macro"] = "添加停止施法（/stopcasting）到宏内（需重载界面）"
 L["Includes the shortest Cooldown in the reset Condition of Castsequence. !!USE CAREFULLY!!"] =
@@ -21,11 +26,15 @@ L["Low Priority Healthstones"] = "降低治疗石使用优先级"
 L["Other / Racial"] = "其他 / 种族"
 L["Potion of Withering Dreams"] = "凋零梦境药水"
 L["Potion of Withering Vitality"] = "枯萎活力药水"
-L["Prioritize health potions over a healthstone."] = "治疗药水优先于治疗石使用"
+L["Prioritize health potions over a healthstone."] = "治疗药水优先于治疗石使用。"
 L["Reset successful!"] = "重置成功！"
 L["Reset to Default"] = "重置为默认"
 L["Shows the bandage that will currently be used, based on what is in your bags."] = 
-"显示当前根据背包物品将会使用的绷带。"
+"显示当前将使用的绷带（背包内物品）。"
+L["Shows the food that will currently be used, based on what is in your bags."] =
+"显示当前将使用的食物（背包内物品）。"
+L["Shows the drink that will currently be used, based on what is in your bags."] =
+"显示当前将使用的饮品（背包内物品）。"
 L["The Settings of AutoPotion were reset due to breaking changes."] = 
 "由于插件结构变动，AutoPotion的设置已被重置。"
 L["Useful for casters."] = "对施法职业有用。"
@@ -33,4 +42,6 @@ L["Useful for casters."] = "对施法职业有用。"
 -- code
 L["AutoPotion"] = "AutoPotion"   -- DO NOT TRANSLATE
 L["AutoBandage"] = "AutoBandage" -- DO NOT TRANSLATE
+L["AutoFood"] = "AutoFood"       -- DO NOT TRANSLATE
+L["AutoDrink"] = "AutoDrink"     -- DO NOT TRANSLATE
 
